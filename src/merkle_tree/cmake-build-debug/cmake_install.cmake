62d3db8497eb275f8c9c7bfb1d24607b5f21c9a4
shimmer147 <wailiheng@gmail.com>
2023-04-16 13:58:38 +0800

    update

--- src/merkle_tree/cmake-build-debug/cmake_install.cmake
+++ src/merkle_tree/cmake-build-debug/cmake_install.cmake
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/Eighthalf/gem5/src/merkle_tree/cmake-build-debug/code/cmake_install.cmake")
+  include("/home/Eighthalf/gem5/src/merkle_tree/cmake-build-debug/src/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
